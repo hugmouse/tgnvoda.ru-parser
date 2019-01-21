a182a3a4131e462bd0ef1b4aaeda4c7aabda16aa
Mouse! <github@hugmouse.com>
2019-01-21 23:58:32 +0800

    Little update

--- parser.php
+++ parser.php
@@ -2,31 +2,43 @@
 require "vendor/autoload.php";
 use DiDom\Document;
 
+function multiexplode ($delimiters,$string) {
+
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $launch = explode($delimiters[0], $ready);
+    return  $launch;
+}
+
 $html = new Document('http://www.tgnvoda.ru/avarii.php', true, 'windows-1251');
 
-// $streets = file('streets.txt');
+$streets = file('streets.txt');
 
 $posts = $html->find('td[bgcolor="#ffffff"]');
 
 foreach($posts as $post) {
+    $array["STREETS"] = '';
+    $array["DATE"] = '';
+    $array["UNTILTIME"] = '';
+    $stopit = '';
     $array["DATE"] = $post->find("font[size=2]")[1]->text();
     $text = $post->child(0)->text();
 
-    // ADDRESS
-    $a = preg_match("/(?<=адресу).*?(\s,)/", $text, $match); 
-    if ($a === 1) {
-       $array["ADDRESS"] = trim($match[0], ":, ");
-    }
-    else {
-        $array["ADDRESS"] = NULL;
+    // let's start very expensive operation!
+    $words = multiexplode([',', '-', ' '], $text);
+    $uniquewords = array_unique($words);
+    foreach($uniquewords as $word) {
+        foreach($streets as $street) {
+            similar_text($street, $word, $perc);
+            if ($perc > 80)
+            {
+                //echo "сходство: $street и $word = $perc%\n";
+                $stopit .= $word.',';
+                break;
+            }
+        }
     }
 
-    // TODO: сделать нормальный поиск улиц. А то ведь они забивают информацию криво.
-    // foreach($streets as $street)
-    // {
-    //     $adress = strstr($text, $street);
-    //     $array["ADDRESS"] .= $adress;
-    // }
+    $array["STREETS"] = substr($stopit, 0, -1);
 
     // TIME
     $b = preg_match("/(([0-9][0-9])(-|ч.)([0-9][0-9]))/", $text, $match2);
